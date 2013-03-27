f633b943c51b0f4e608d7b8e26b848eca0aa8bb9
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-03-26 21:23:09 -0400

    Add routes resources entry for action_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 LifeAdvisor::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  resources :action_items
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
