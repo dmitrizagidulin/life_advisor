ecdd24fdb94a8e338fb13b2aa90b7a0e1ac74b62
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-03-26 20:22:58 -0400

    Stock rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeAdvisor::Application.config.secret_token = '506b7f38e47ede9a5525576be34c375c6ba7b0ce21db506133af33f3abc06b50ecb44dcb463d9723ca5b4236df9e3c1e71904d9e16dd60c2a7ed280152875ab2'
