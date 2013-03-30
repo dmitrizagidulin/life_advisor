109fd1530fd252de8e7d3cffcef9538bed75019a
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-03-30 18:00:48 -0400

    Add 'Done' checkbox to action item list views, convert done from string to bool in model

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -23,6 +23,7 @@ class ActionItem
   def self.from_search_result(document)
     fields = document['fields']
     fields.delete('_type') # Remove the _type key, gives an error when instantiating ActionItem
+    fields['done'] = fields['done'] == 'true'
     action_item = ActionItem.new(fields)
     action_item.key = document['id']
     action_item
