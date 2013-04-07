6261d32299182226e2e1593c4de597c75981153e
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-04-06 23:26:06 -0400

    Add 'all_for_status' method to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,9 +6,13 @@ class Project
   
   property :name, String, :presence => true
   property :description, String
-  property :status, String, :default => :idea  # One of [:idea, :active, :someday, :cancelled, :completed]
+  property :status, String, :default => :idea  # One of [:idea, :active, :someday, :canceled, :completed]
   
   timestamps!
   
-  
+  def self.all_for_status(status)
+    search_string = "status:"+status.to_s
+    results = self.search_results_for(search_string)
+    results.collect { |doc| self.from_search_result(doc) }
+  end
 end
