ed30ec37744c3fe00d911776843d9eadffac9045
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-05-18 12:58:31 -0400

    Set default route to action_items

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ LifeAdvisor::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'action_items#index'
 
   # See how all your routes lay out with "rake routes"
 
