3bc164084c2eb2126a8dd9e84db3466b5192e2fb
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-10-18 00:07:44 -0400

    Generate Question model & controller scaffold

---
+++ app/models/question.rb
@@ -0,0 +1,12 @@
+require 'RippleSearch'
+
+class Question
+  include Ripple::Document
+  extend RippleSearch
+  include Parentable
+    
+  property :name, String, :presence => true
+  property :answered_at, Time
+  property :description, String
+  timestamps!
+end
