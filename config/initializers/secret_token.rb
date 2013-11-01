4e277e3c69335cb8fad871c7a1d51f4a1b5517ab
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-11-01 14:53:32 -0400

    Rails 4: rails new . -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifeAdvisor::Application.config.secret_key_base = 'db648769d5c0b64bc94d041dccee13c3fc299144f27c350d3372747b48e3bac337e748783d632bfcc4d0eb0b34920ae08769b44841eb5995d1043ec2b4d21162'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LifeAdvisor::Application.config.session_store :cookie_store, key: '_life_advisor_session'
