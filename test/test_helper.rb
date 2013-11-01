ed0189333e0f87ecdc9df6af9d6ba6b44d2ed676
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-11-01 15:05:08 -0400

    rails g ripple

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,20 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Setup in-memory test server for Riak
+
+
+require 'ripple/test_server'
+
+
+
 class ActiveSupport::TestCase
+
+
+  setup { Ripple::TestServer.setup }
+
+
+  teardown { Ripple::TestServer.clear }
+
   # Add more helper methods to be used by all tests here...
 end
