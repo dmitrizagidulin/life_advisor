518d1d464fc144983119640d9b2282b44e00a075
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-11-15 10:16:10 -0800

    Style list, show, edit screens for People

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -69,6 +69,6 @@ class PeopleController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def person_params
-      params[:person]
+      params[:person].permit(:name, :description, :parent_key, :parent_type)
     end
 end
